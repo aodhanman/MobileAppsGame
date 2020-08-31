4d92316e3e55662094ec807a0577093ba4912c98
AODHAN MANNION O DONNELL <G00314829@gmit.ie>
2020-08-31 18:01:42 +0100

    Revert "Level changing and boss working"
    This reverts commit 00b0dc99b170de0f60eae438e9557f3e6a39a097.

---
+++ Shooter/Assets/Scripts/KeepScore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepScore : MonoBehaviour
+{
+    public static int Score = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnGUI()
+    {
+        GUI.Box (new Rect (20, 20, 50, 50), Score.ToString());
+        
+    }
+
+}
+
